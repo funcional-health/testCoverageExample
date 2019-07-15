6fb33cefee3163b132b2ff6b45168d12eb8d61a6
Victor Machado <victor_w.tkd@hotmail.com>
2019-07-14 22:53:34 -0300

    Criando o primeiro teste

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -4,5 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-
+    public function testFizzBuzzShouldReturnFizzForEveryMultipleOf3()
+    {
+        $fizzBuzz = new FizzBuzz();
+	$this->assertEquals("Fizz", $fizzBuzz->processNumber(9));
+    }
 }
