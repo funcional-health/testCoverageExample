47a888225001086deab3615cbcc08f38f70b0814
Renato Filizzola <rfilizzola@funcionalcorp.com.br>
2020-08-27 15:23:57 -0300

    #TEST-03 Ajustando indentacao

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -2,18 +2,18 @@
 
 class FizzBuzz
 {
-    public function processNumber ($number)
+    public function processNumber($number)
     {
         $result = "";
 
-        if ($number%3 == 0) {
+        if ($number % 3 == 0) {
             $result .= "Fizz";
-	}
+        }
 
-	if ($number%5 == 0) {
-             $result .= "Buzz";
-	}
+        if ($number % 5 == 0) {
+            $result .= "Buzz";
+        }
 
-	return $result;
+        return $result;
     }
 }
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,18 +7,18 @@ class FizzBuzzTest extends TestCase
     public function testFizzBuzzShouldReturnFizzForEveryMultipleOf3()
     {
         $fizzBuzz = new FizzBuzz();
-	$this->assertEquals("Fizz", $fizzBuzz->processNumber(9));
+        $this->assertEquals("Fizz", $fizzBuzz->processNumber(9));
     }
 
     public function testFizzBuzzShouldReturnBuzzForEveryMultipleOf5()
     {
         $fizzBuzz = new FizzBuzz();
-	$this->assertEquals("Buzz", $fizzBuzz->processNumber(25));
+        $this->assertEquals("Buzz", $fizzBuzz->processNumber(25));
     }
 
     public function testFizzBuzzShouldReturnFizzBuzzForEveryMultipleOf3And5()
     {
         $fizzBuzz = new FizzBuzz();
-	$this->assertEquals("FizzBuzz", $fizzBuzz->processNumber(225));
+        $this->assertEquals("FizzBuzz", $fizzBuzz->processNumber(225));
     }
 }
