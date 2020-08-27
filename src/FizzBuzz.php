5626de17181c3591b1a1599847a9322342062165
Victor Machado <vmachado@funcionalcorp.com.br>
2020-08-27 11:40:11 -0300

    criando metodos com ternarios

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -2,18 +2,26 @@
 
 class FizzBuzz
 {
-    public function processNumber ($number)
+    public function processNumber ($number) : string
     {
         $result = "";
 
-        if ($number%3 == 0) {
-            $result .= "Fizz";
-	    }
+        $result .= $this->divisibleBy3($number);
 
-	    if ($number%5 == 0) {
-             $result .= "Buzz";
-	    }
+        $result .= $this->divisibleBy5($number);
 
 	    return $result;
+    }
+
+    public function divisibleBy3 (int $number) : string
+    {
+        $result = ($number%3 == 0) ? "Fizz" : "";
+        return $result;
+    }
+
+    public function divisibleBy5 (int $number) : string
+    {
+        $result = ($number%5 == 0) ? "Buzz" : "";
+        return $result;
     }
 }
